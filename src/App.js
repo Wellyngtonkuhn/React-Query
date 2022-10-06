8fa7fd325d09238f816a2de75acfde380075f25a
Wellyngton Antonio Kuhn <94876959+Wellyngtonkuhn@users.noreply.github.com>
2022-10-06 16:35:29 -0300

    Fazendo consulda de apiRest com React Query

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import './App.css';
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import "./App.css";
 
 function App() {
+  const [id, setId] = useState(1);
+
+  const { data, isLoading, isError } = useQuery(["user", id], async () => {
+    const request = await fetch(`https://reqres.in/api/users/${id}?delay=1`);
+    const response = await request.json();
+
+    return response.data;
+  });
+
   return (
-    <div className="App">
-     teste
-    </div>
+    <section className="App">
+      <div className="content">
+        {isLoading && <p>Loading...</p>}
+        {isError && (
+          <p>
+            Um error aconteceu, recarregue a página por favor ou tente mais
+            tarde
+          </p>
+        )}
+        {data && (
+          <>
+            <img src={data.avatar} alt={data.first_name} />
+            <p>
+              {data.first_name} {data.last_name}
+            </p>
+            <p>Email: {data.email}</p>
+            <div>
+              <button onClick={() => setId(id - 1)}>Prev</button>
+              <button onClick={() => setId(id + 1)}>Next</button>
+            </div>
+          </>
+        )}
+      </div>
+    </section>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<App />
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
 );
